6254e4b9dacf93b2e2893b2feafff044d5c4a96b
Kendo <osataken@gmail.com>
2015-03-03 18:08:08 +0700

    adjust configuration for s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,8 @@ Rails.application.configure do
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
+
+  # config/initializers/paperclip.rb
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-website-ap-southeast-1.amazonaws.com'
+Paperclip::Attachment.default_options[:path] = '/:class/:attachment/:id_partition/:style/:filename'
 end
